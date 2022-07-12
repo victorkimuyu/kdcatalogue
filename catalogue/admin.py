e98c2938509b1fb5eb49bc9a843c9856f055453c
kimuyu <victorkimuyu@gmail.com>
2022-07-12 06:39:59 +0300

    Added Kayak model admin

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,15 @@
+from atexit import register
 from django.contrib import admin
+from .models import Kayak, Feature
 
-# Register your models here.
+class FeatureInline(admin.StackedInline):
+    model = Feature
+admin.site.register(Feature)
+
+class KayakAdmin(admin.ModelAdmin):
+    model = Kayak
+    list_display = ['brand', 'model', 'build', 'description', 'length', 'width', 'height', 'weight', 'load_capacity']
+    list_filter = ['brand', 'build', 'steering', 'paddling', 'is_new', 'in_stock']
+    inlines = (Feature,)
+
+admin.site.register(Kayak)
--- catalogue/models.py
+++ catalogue/models.py
@@ -7,24 +7,24 @@ class Feature(models.Model):
 
 class Kayak(models.Model):
     class BRAND_CHOICES(models.TextChoices):
-        AZUL = 'AZ', 'Axul Kayaks'
-        COBRA = 'CB', 'Cobra Kayaks'
-        RIOT = 'RT', 'Riot Kayaks'
-        BOREAL = 'BR', 'Boreal Design'
+        AZUL = "AZ", "Azul Kayaks"
+        COBRA = "CB", "Cobra Kayaks"
+        RIOT = "RT", "Riot Kayaks"
+        BOREAL = "BR", "Boreal Design"
 
     class PADDLING_CHOICES(models.TextChoices):
-        SOLO = 'S', 'Solo'
-        TANDEM = 'T', 'Tandem'
+        SOLO = "S", "Solo"
+        TANDEM = "T", "Tandem"
 
     class STEERING_CHOICES(models.TextChoices):
-        SOLO = 'RUDDER', 'Rudder'
-        TANDEM = 'SKEG', 'Skeg'
+        SOLO = "RUDDER", "Rudder"
+        TANDEM = "SKEG", "Skeg"
 
-    brand = models.CharField(max_length=20)
-    model_name = models.CharField(
-        "Model", max_length=50, primary_key=True, choices=BRAND_CHOICES.choices)
+    brand = models.CharField(max_length=20, choices=BRAND_CHOICES.choices)
+    model_name = models.CharField("Model", max_length=50, primary_key=True)
     build = models.CharField(max_length=50)
     description = models.TextField()
+    key_features = models.ForeignKey(Feature, on_delete=models.SET_NULL, null=True, blank=True)
     model_code = models.CharField("Code", max_length=20)
     web_page = models.URLField(max_length=255, unique=True)
     youtube = models.URLField(max_length=50)
@@ -35,17 +35,19 @@ class Kayak(models.Model):
     paddling = models.CharField(
         "Paddling", max_length=20, choices=PADDLING_CHOICES.choices
     )  # Solo / Tandem
-
-    is_new = models.BooleanField(default=False)
-    in_stock = models.BooleanField(default=True)
-    discontinued = models.BooleanField(default=True)
-
+    
     # Dimensions
     length = models.FloatField()
     width = models.FloatField()
     height = models.FloatField()
     weight = models.FloatField()
     load_capacity = models.CharField(max_length=7)
+    outer_cockpit_dimensions = models.CharField(max_length=50, null=True, blank=True)
+    ideal_paddler_size = models.PositiveIntegerField(null=True, blank=True)
+    beluga_skirt_size = models.CharField(max_length=10, null=True, blank=True)
+
+    is_new = models.BooleanField(default=False)
+    in_stock = models.BooleanField(default=True)
 
     def __str__(self):
         return self.model_name
