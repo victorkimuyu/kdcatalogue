51c798bef7b8d527344cbd204c4ecb99f82fad57
kimuyu <victorkimuyu@gmail.com>
2022-07-12 14:10:09 +0300

    Created photo StackedInline

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,15 +1,15 @@
 from atexit import register
 from django.contrib import admin
-from .models import Kayak, Feature
+from .models import Kayak, Photo
 
-class FeatureInline(admin.StackedInline):
-    model = Feature
-admin.site.register(Feature)
+class PhotoInline(admin.StackedInline):
+    model = Photo
 
 class KayakAdmin(admin.ModelAdmin):
     model = Kayak
     list_display = ['brand', 'model', 'build', 'description', 'length', 'width', 'height', 'weight', 'load_capacity']
     list_filter = ['brand', 'build', 'steering', 'paddling', 'is_new', 'in_stock']
-    inlines = (Feature,)
+    search_fields = ['brand', 'model', 'build', 'load_capacity', 'weight']
+    inlines = [PhotoInline]
 
-admin.site.register(Kayak)
+admin.site.register(Kayak, KayakAdmin)
