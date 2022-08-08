425e568093ca7f924f738f40cb592027cd408277
kimuyu <victorkimuyu@gmail.com>
2022-08-08 08:35:02 +0300

    Added Riot, Azul and Boreal models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,50 +1,66 @@
 from django.db import models
 
 
+class KayakManager(models.Manager):
+    pass
+
+
 class Kayak(models.Model):
-    class BRAND_CHOICES(models.TextChoices):
+    class BrandChoices(models.TextChoices):
         AZUL = "AZ", "Azul Kayaks"
         COBRA = "CB", "Cobra Kayaks"
         RIOT = "RT", "Riot Kayaks"
         BOREAL = "BR", "Boreal Design"
 
-    class PADDLING_CHOICES(models.TextChoices):
+    class PaddlingChoices(models.TextChoices):
         SOLO = "S", "Solo"
         TANDEM = "T", "Tandem"
 
-    class STEERING_CHOICES(models.TextChoices):
-        SOLO = "RUDDER", "Rudder"
-        TANDEM = "SKEG", "Skeg"
+    class SteeringChoices(models.TextChoices):
+        SOLO = "R", "Rudder"
+        TANDEM = "S", "Skeg"
 
-    brand = models.CharField(max_length=20, choices=BRAND_CHOICES.choices)
-    model_name = models.CharField("Model", max_length=50, primary_key=True)
-    build = models.CharField(max_length=50)
-    description = models.TextField()
+    brand = models.CharField(verbose_name="Brand", max_length=20,
+                             null=True,
+                             blank=True, choices=BrandChoices.choices)
+    model_name = models.CharField(verbose_name="Model", max_length=50)
+    material = models.CharField(verbose_name="Material", max_length=50, null=True, blank=True)
+    description = models.TextField(verbose_name="Description")
     key_features = models.TextField(null=True, blank=True)
-    model_code = models.CharField("Code", max_length=20)
-    web_page = models.URLField(max_length=255, unique=True)
-    youtube = models.URLField(max_length=50)
+    model_code = models.CharField(verbose_name="Code", max_length=20)
+    web_page = models.URLField(max_length=255,
+                               null=True,
+                               blank=True)
+    youtube = models.URLField(verbose_name="Youtube", max_length=50, null=True, blank=True)
 
     steering = models.CharField(
-        "Steering", max_length=20, choices=STEERING_CHOICES.choices
-    )  # Skeg / Rudder
+        verbose_name="Steering",
+        max_length=20,
+        choices=SteeringChoices.choices, blank=True, null=True
+    )
     paddling = models.CharField(
-        "Paddling", max_length=20, choices=PADDLING_CHOICES.choices
-    )  # Solo / Tandem
+        verbose_name="Paddling",
+        max_length=20,
+        choices=PaddlingChoices.choices,
+        null=True,
+        blank=True,
+    )
 
     # Dimensions
-    length = models.FloatField()
-    width = models.FloatField()
-    height = models.FloatField()
-    weight = models.FloatField()
-    load_capacity = models.CharField(max_length=7)
-    outer_cockpit_dimensions = models.CharField(
-        max_length=50, null=True, blank=True)
-    ideal_paddler_size = models.PositiveIntegerField(null=True, blank=True)
-    beluga_skirt_size = models.CharField(max_length=10, null=True, blank=True)
+    length = models.CharField(max_length=10, verbose_name="Length", null=True, blank=True)
+    width = models.CharField(max_length=10, verbose_name="Width", null=True, blank=True)
+    height = models.CharField(max_length=10, verbose_name="Height", null=True, blank=True)
+    weight = models.CharField(max_length=50, verbose_name="Weight", null=True, blank=True)
+    load_capacity = models.CharField(max_length=50, verbose_name="Load Capacity", null=True, blank=True)
+    outer_cockpit_dimensions = models.CharField(max_length=50,
+                                                null=True,
+                                                blank=True)
+    ideal_paddler_size = models.CharField(max_length=20, verbose_name="Paddler Size", null=True, blank=True)
+    beluga_skirt_size = models.CharField(max_length=20, verbose_name="Beluga Skirt", null=True, blank=True)
+    impulse_drive = models.CharField(verbose_name="Impulse Drive", max_length=50, null=True, blank=True)
 
-    is_new = models.BooleanField(default=False)
-    in_stock = models.BooleanField(default=True)
+    is_new = models.BooleanField(verbose_name="New", default=False, null=True, blank=True)
+    in_stock = models.BooleanField(verbose_name="In Stock", default=True, null=True, blank=True)
 
     def __str__(self):
         return self.model_name
@@ -52,7 +68,10 @@ class Kayak(models.Model):
 
 class Photo(models.Model):
     image = models.ImageField(upload_to="../static/boat_photos")
-    Kayak = models.ForeignKey(Kayak, on_delete=models.SET_NULL, null=True, blank=True)
+    Kayak = models.ForeignKey(Kayak,
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              blank=True)
 
     def __str__(self):
-        return self.image.path.split('\\')[-1]
+        return self.image.path.split("\\")[-1]
---
+++ data/boreal.py
@@ -0,0 +1,104 @@
+model_names = [
+    "BALTIC 120",
+    "KASKO Rudder",
+    "OOKPIK Rudder",
+    "SEDNA Rudder",
+    "MUKTUK Rudder",
+    "INUKSHUK Rudder",
+    "BAFFIN P1 Skeg",
+    "BAFFIN P2 Skeg",
+    "BAFFIN P3 Skeg",
+    "EPSILON P100 Rudder",
+    "EPSILON P200 Rudder",
+    "EPSILON P300 Rudder",
+    "ESPERANTO Rudder Heavy duty",
+    "BALTIC 120 TX",
+    "PURA 120 TX",
+    "HALO 130 TX",
+    "COMPASS 140 TX",
+    "STORM 15 Rudder",
+    "STORM 16 Rudder UL",
+    "STORM 17 Rudder",
+    "BAFFIN T2 Skeg",
+    "EPSILON T200 Rudder",
+    "EPSILON T200 Rudder UL",
+    "PASSAGE TX (Tandem)",
+]
+
+materials = [
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Rotomolded HDPE",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+    "Thermoformed TX",
+]
+
+descriptions = [
+"The Baltic with its Swede form hull is exceptionally fast and smooth with every stroke of the paddle.The Baltic fits larger paddlers with ample room for knee movement to allow for blood flow to the legs and with our amazing comfortable Flex 4 seat adjustments on back and leg lift front, there is nothing else to want on a long trip.",
+
+"The Kasko is a very agile recreational kayak. It’s ideally suited to exploring winding rivers, lakes, and coastlines. With a smaller coaming, the Kasko will appeal to smaller sized paddlers. ",
+
+"The Ookpik is a very agile recreational kayak. It is ideally suited to exploring winding rivers, lakes, and coastlines. With a larger coaming, the Ookpik will appeal to mid to karge-sized paddlers. Check out the Kasko if you are looking for a tighter fitting kayak.",
+
+"The Sedna is one of our most versatile models with arched hull shape and waterline length to provide stability and predictable behavior. The Sedna is easy to handle, requiring minimum effort to maintain a good cruising speed. These characteristics are sure to appeal to the day or weekend paddler who love to go exploring in all weather and water conditions, without needing the storage necessary for a 17’ kayak.",
+
+"The Muktuk’s low profile offers plenty of thigh support, making it ideal for small to medium-sized paddlers. Its shape generates minimal wind resistance, great storage and the kayak is highly responsive during turns. A tapered contour adds speed to a craft that is stable, comfortable, and easy to paddle.",
+
+"The Inukshuk combines elegance and performance. It is unquestionably the fastest kayak in its category. Its long waterline and tapered contour allow the kayak to accelerate quickly and track beautifully, while the up-swept bow provides buoyancy when paddling in waves. A moderate rocker makes the kayak very agile while performing a lean turn. A high back deck provides ample storage for extended touring.",
+
+"The Baffin is built for the rigors of rough seas. Its unique hull design provides paddlers with a lively yet stable boat compared to other Greenland style kayaks. The three models in the series are well suited for a wide range of paddlers who demand immediate control and predictability. The generous sized opening of the hatch allows paddlers to load up adequate amounts of gear for up to two weeks, while the day hatch keeps your essentials handy while on the water.",
+
+"The Baffin is built for the rigors of rough seas. Its unique hull design provides paddlers with a lively yet stable boat compared to other Greenland style kayaks. The three models in the series are well suited for a wide range of paddlers who demand immediate control and predictability. The generous sized opening of the hatch allows paddlers to load up adequate amounts of gear for up to two weeks, while the day hatch keeps your essentials handy while on the water.",
+
+"The Baffin is built for the rigors of rough seas. Its unique hull design provides paddlers with a lively yet stable boat compared to other Greenland style kayaks. The three models in the series are well suited for a wide range of paddlers who demand immediate control and predictability. The generous sized opening of the hatch allows paddlers to load up adequate amounts of gear for up to two weeks, while the day hatch keeps your essentials handy while on the water.",
+
+"The Epsilon is the top selling series for tour operators and rental fleets. This stable and predictable craft works with a wide variety of paddlers. With our easily adjustable foot sliders, the Epsilon accommodates a wide size range of paddlers, tall or short and up to 300 lbs. The Epsilon is a smooth, predictable and stable craft. Even in rougher water the ride is quite dry. Load up for a couple weeks and enjoy the speed of the Epsilon. Simply a delightful kayak to paddle while being able to count on its dependability!",
+
+"The Epsilon is the top selling series for tour operators and rental fleets. This stable and predictable craft works with a wide variety of paddlers. With our easily adjustable foot sliders, the Epsilon accommodates a wide size range of paddlers, tall or short and up to 300 lbs. The Epsilon is a smooth, predictable and stable craft. Even in rougher water the ride is quite dry. Load up for a couple weeks and enjoy the speed of the Epsilon. Simply a delightful kayak to paddle while being able to count on its dependability!",
+
+"The Epsilon is the top selling series for tour operators and rental fleets. This stable and predictable craft works with a wide variety of paddlers. With our easily adjustable foot sliders, the Epsilon accommodates a wide size range of paddlers, tall or short and up to 300 lbs. The Epsilon is a smooth, predictable and stable craft. Even in rougher water the ride is quite dry. Load up for a couple weeks and enjoy the speed of the Epsilon. Simply a delightful kayak to paddle while being able to count on its dependability!",
+
+"The Esperanto is one of the fastest plastic tandems on the market. It is easy to paddle, comfortable, stable, and can reach impressive cruising speeds. A moderate rocker and a large rudder contribute to efficient turns. The practical center compartment can be used for extra storage or outfitted with our optional child seat/bulkhead. The cockpits are spaced sufficiently apart, allowing the paddlers to paddle at their own speed without risk of accidental collision between paddles.Ideally suited to a pair of paddlers with mixed abilities, families with young children, or a long expedition needing high volume of storage and displacement.",
+
+"The Baltic with its Swede form hull is exceptionally fast and smooth with every stroke of the paddle.The Baltic fits larger paddlers with ample room for knee movement to allow for blood flow to the legs and with our amazing comfortable Flex 4 seat adjustments on back and leg lift front, there is nothing else to want on a long trip.",
+
+"The Pura 120 is the perfect recreational kayak for beginners or those looking for a stress free paddle. Heavy on style but easy to handle, both on and off the water. The Pura 120 is a joy to paddle. Stable, responsive and comfortable, this versatile kayak will let you explore with confidence. Inspired by the very latest in advanced hull design, our designer has crafted a fast, stable kayak that tracks effortlessly & ensures a “dry ride”. The Pura 120 is already earning the trust of seasoned paddlers.",
+
+"Designed to give kayakers comfort and stability with agile yet reassured handling, the Halo 130 will make all of your adventures memorable. Experienced paddlers and those looking to develop their skills will appreciate the Halo 130’s potential. This is truly a light and fun kayak! Inspired by the latest in advanced hull design, Boreal Design has crafted a fast, stable kayak that tracks effortlessly and ensures a “dry ride”. Rough Water Ready – Beginner Friendly!"
+                                                                                                                                                                                        
+"Fast, sleek, stable and seaworthy – the Compass 140 does it all. While not a full touring kayak, this craft gives you options. With enough storage capacity for an overnight trip, yet light enough to easily carry, this is a versatile recreational kayak. The Compass 140 will turn on a dime, handle rough water and more importantly, put a smile on your face. The ergonomic cockpit with our industry leading 5-Way Adjustable Seat will fit a wide range of paddlers. The new day hatch is within easy reach, along with the optional rudder.",
+
+"The Storm 15 is a kayak with it’s multi-chine hull, that can do many things really well. For those small to mid-sized paddlers looking for a light-weight, durable, and comfortable boat that’s versatile, the Storm 15 ticks all the boxes. Whether you’re heading out on a long trip or a short skip across the bay, this kayak is stable, easily packable and very seaworthy. Built with our Thermoformed ABS, this will be your favourite kayak for many years to come.",
+
+"The Storm 16 is a kayak with it’s multi-chine hull, that can do many things really well. For those mid-sized paddlers looking for a light-weight, durable, and comfortable boat that’s versatile, the Storm 16 ticks all the boxes. Whether you’re heading out on a long trip or a short skip across the bay, this kayak is stable, easily packable and very seaworthy. Built with our Thermoformed ABS / Ultralight, this will be your favourite kayak for many years to come.",
+
+"The Storm 17 is a kayak with it’s multi-chine hull, that can do many things really well. For those mid-sized to larger paddlers looking for a light-weight, durable, and comfortable boat that’s versatile, the Storm 17 ticks all the boxes. Whether you’re heading out on a long trip or a short skip across the bay, this kayak is stable, easily packable and very seaworthy. Built with our Thermoformed ABS, this will be your favourite kayak for many years to come.",
+
+"The Baffin is built for the rigors of rough seas. Its unique hull design provides paddlers with a lively yet stable boat compared to other Greenland style kayaks. The three models in the series are well suited for a wide range of paddlers who demand immediate control and predictability. The generous sized opening of the hatch allows paddlers to load up adequate amounts of gear for up to two weeks, while the day hatch keeps your essentials handy while on the water.",
+
+"The Epsilon is the top selling series for tour operators and rental fleets. This stable and predictable craft works with a wide variety of paddlers. With our easily adjustable foot sliders, the Epsilon accommodates a wide size range of paddlers, tall or short and up to 300 lbs. The Epsilon is a smooth, predictable and stable craft. Even in rougher water the ride is quite dry. Load up for a couple weeks and enjoy the speed of the Epsilon. Simply a delightful kayak to paddle while being able to count on its dependability!",
+
+"The Epsilon is the top selling series for tour operators and rental fleets. This stable and predictable craft works with a wide variety of paddlers. With our easily adjustable foot sliders, the Epsilon accommodates a wide size range of paddlers, tall or short and up to 300 lbs. The Epsilon is a smooth, predictable and stable craft. Even in rougher water the ride is quite dry. Load up for a couple weeks and enjoy the speed of the Epsilon. Simply a delightful kayak to paddle while being able to count on its dependability!",
+
+"The Passage is a speedy, light weight, tandem kayak with stable hull and is comfortable for new and experienced paddlers. We have developed the cockpits to accommodate paddlers of all sizes. The outstanding comfort of the Passage is excellent for an afternoon cruise or a multiday trip. The Passage is not only incredibly stable – ideal for fishing, photography and wildlife viewing – but has a long waterline length, astounding volume, “dry ride” and comfort. The Passage is a capable long range cruiser yet remains a fun kayak for an afternoon paddle. An excellent value at an affordable price!",
+
+]
