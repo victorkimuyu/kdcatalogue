fb1249191007a9483bc98c619d6073f3ab1dc41a
kimuyu <victorkimuyu@gmail.com>
2022-08-29 16:39:18 +0300

    Various improvements

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -6,9 +6,11 @@ from .models import Kayak
 class KayakAdmin(admin.ModelAdmin):
     model = Kayak
     exclude = ['slug']
-    list_display = ['brand', 'model_name', 'material', 'description', 'key_features', 'length', 'width', 'height',
-                    'weight',
-                    'load_capacity']
+    # list_display = ['brand', 'model_name', 'material', 'description', 'key_features', 'length', 'width', 'height',
+    #                 'weight',
+    #                 'load_capacity']
+    list_display = ['brand', 'model_name', 'top_view']
+    list_editable = ['top_view']
     # list_filter = ['brand', 'material', 'steering', 'paddling', 'is_new', 'in_stock']
     search_fields = ['brand', 'model_name', 'material', 'load_capacity', 'weight']
     list_display_links = ['model_name']
