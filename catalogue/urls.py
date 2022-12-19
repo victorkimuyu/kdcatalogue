9dc7a3e605112882de4a68c9cbf76789084b99e3
Victor Kimuyu <52920061+victorkimuyu@users.noreply.github.com>
2022-12-19 12:38:23 +0300

    Ran collectstatic

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
                   path('', Kayaks.as_view(), name='index'),
-                  path('kayaks/new/', KayakCreate.as_view(), name='kayak-create'),
-                  path('kayaks/<slug:slug>/update/', KayakUpdate.as_view(), name='kayak-update'),
-                  path('kayaks/<slug:slug>/', KayakDetail.as_view(), name='kayak-detail'),
-                  path('kayaks/', KayakList.as_view(), name='kayak-list'),
+                  path('admin/kayaks/new/', KayakCreate.as_view(), name='kayak-create'),
+                  path('admin/kayaks/<slug:slug>/update/', KayakUpdate.as_view(), name='kayak-update'),
+                  path('admin/kayaks/<slug:slug>/', KayakDetail.as_view(), name='kayak-detail'),
+                  path('admin/kayaks/', KayakList.as_view(), name='kayak-list'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
